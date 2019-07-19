187f128b178ebd79bb87bc698726c0a4701c38c3
keerthitp_git <tp.keerthana.k@gmail.com>
2019-07-19 09:30:56 -0500

    added math methods and overloaded 2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,29 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello world");
+
+
+    }
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int add(int a, int b, int c){
+        return a+b+c;
+    }
+
+    public int subtract(int a, int b){
+        return a-b;
+    }
+    public int subtract(int a, int b, int c){
+        return a-b-c;
+    }
+
+    public int multiply(int a, int b){
+        return a*b;
+    }
+    public int divide(int a, int b){
+        return a/b;
     }
 }
